bc73c68bc9bdd7a9b07127afa61a3311a84964aa
Asif mia <asifmia.dev26@gmail.com>
2025-05-24 10:51:37 +0600

    Add randomNumber.js file
    _ Thsi file generate random number between 1 to 6
    
    _ Using switch case track ludospin

---
+++ randomNumber.js
@@ -0,0 +1,21 @@
+const ludoSprin = Math.round(Math.random()*6);
+
+switch(ludoSprin){
+    case 1:
+        console.log("You can move",ludoSprin,"squre");
+        break;
+     case 2:
+        console.log("You can move",ludoSprin," squre");
+        break;
+     case 3:
+        console.log("You can move",ludoSprin," squre");
+        break;
+     case 4:
+        console.log("You can move",ludoSprin," squre");
+        break;
+     case 5:
+        console.log("You can move",ludoSprin," squre");
+        break;
+    default:
+        console.log("You can move",ludoSprin," squre");
+}
